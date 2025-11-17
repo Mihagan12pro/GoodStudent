4b29fa1e6e51548e44d87fe4117e441c7cdbf068
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-11-17 19:58:08 +0300

    feat: add the ISqlConnectionFactory interface.

---
+++ src/Server/GoodStudent.Infrastructure.Postgre/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using GoodStudent.Application;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodStudent.Infrastracture.Postgres
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPostgresInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<ISqlConnectionFactory, SqlConnectionFactory>();
+            //services.AddScoped<I>
+
+            return services;
+        }
+    }
+}
---
+++ src/Server/GoodStudent.Infrastructure.Postgre/SqlConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using GoodStudent.Application;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodStudent.Infrastracture.Postgres
+{
+    internal class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        public IDbConnection CreateConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
