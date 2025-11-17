83008537460c39aae7a0c17be0db5d0418254322
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-11-17 14:12:53 +0300

    refactor: add the DependencyInjection class to the GoodStudent.Web project.

---
+++ src/Server/GoodStudent.Web/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿namespace GoodStudent.Web
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddProgramDependencies(this IServiceCollection services)
+        {
+            services.AddWebDependencies();
+
+            return services;
+        }
+
+        private static IServiceCollection AddWebDependencies(this IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
