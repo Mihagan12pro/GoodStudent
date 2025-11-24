7c459b812b61a57e7b94050288d92af75f380be2
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-11-24 17:54:38 +0300

    feat: can get departments by FacultyId.

---
+++ src/Server/GoodStudent.Presenters/Sections/ProfessionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoodStudent.Presenters.Sections
+{
+    [ApiController]
+    [Route("api/sections/[controller]")]
+    public class ProfessionController : ControllerBase
+    {
+    }
+}
